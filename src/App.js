b319f292e726639872d0dc9fd0c8f6e13903ceec
sourabh321joshi <sourabh321joshi@gmail.com>
2024-05-13 15:10:12 +0530

    signIn/signUp form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
-
+import Body from "./components/Body";
 function App() {
   return (
-    <div className="font-bold ">
-     <p>Namaste react</p>
-    </div>
-  );
+   <Body/>
+);
 }
 
 export default App;
